278aae05bda35c92a68a3dc7057a1d8f2de4fe85
Matheus Pfeifer <matheuspfeifer2@gmail.com>
2023-04-13 03:52:05 -0300

    V1

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react'
+import {Text,Button,View,StyleSheet} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function App() {
+export default function App(){
+  const [contador,setcontador]=useState(0)
+
+  useEffect(() => {
+    loadCount();
+  }, []);
+  
+  const loadCount = async () => {
+    try {
+      const savedCount = await AsyncStorage.getItem('contador');
+      if (savedCount !== null) {
+        setcontador(parseInt(savedCount));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const saveCount = async () => {
+    try {
+      await AsyncStorage.setItem('contador', contador.toString());
+      console.log('Contador salvo com sucesso!');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSave = () => {
+    saveCount();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <View style={styles.conteiner}>
+   <Text style={styles.Texto}>{contador}</Text>
+   <Button  
+   title='Clica em mim' 
+   onPress={()=>setcontador(contador+1)} />
+   <View style={styles.espaco}></View>
+   <Button  
+   title='Salvar' 
+   onPress={handleSave} />
+   </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles=StyleSheet.create({
+  conteiner:{
+  marginTop:250,
+  flex:1,
+  padding:30,
+  alignItems:'center',
   },
-});
+  Texto:{
+  fontSize:50,
+  padding:40,
+  },
+  espaco:{
+  padding:10,
+  },
+})
---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Contador</h1>
+<br>
+<h2>Tecnologias usadas:</h2>
+<table>
+<td>JavaScript</td>
+</table>
+<br>
+<h2>Sobre</h2>
+Neste projeto em React Native eu aprendi a como armazenar valores através do AsyncStorage.
+<br>
+<h2>Pré-requisitos</h2>
+Instale o VScode na sua máquina.
+<h2>Autoria</h2>
+Matheus Pfeifer<br>
+Meu Linkedin --> https://www.linkedin.com/in/matheus-pfeifer/
